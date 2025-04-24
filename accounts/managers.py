763f94923a9606006547c265a55b9cf08d7c600f
mohammad <arabamerimohammad@gmail.com>
2025-04-24 20:49:09 +0330

    add Cors package

--- accounts/managers.py
+++ accounts/managers.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.models import BaseUserManager
 
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
 
 class UserManager(BaseUserManager):
-    def create_user(self, national_code, email, phone_num, full_name, father_name, date_of_birth, address, work_address, password=None):
+    def create_user(self, national_code, email, phone_num, full_name, father_name, date_of_birth, address, work_address,
+                    password=None, user_image=None):
         if not national_code:
             raise ValueError("MyUser must have an National Code Number")
         if not email:
@@ -18,26 +22,32 @@ class UserManager(BaseUserManager):
             father_name=father_name,
             date_of_birth=date_of_birth,
             address=address,
-            work_address=work_address
+            work_address=work_address,
+            user_image=user_image
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, national_code, email, phone_num, full_name, password=None, father_name=None, date_of_birth=None, address=None, work_address=None):
+    def create_superuser(self, national_code, email, phone_num, full_name, password=None, father_name=None,
+                         date_of_birth=None, address=None, work_address=None, profile_image=None):
         user = self.create_user(
             national_code=national_code,
             email=email,
             phone_num=phone_num,
             full_name=full_name,
-            father_name=father_name if father_name else "",
+            father_name=father_name or "",
             date_of_birth=date_of_birth,
-            address=address if address else "",
-            work_address=work_address if work_address else "",
-            password=password
+            address=address or "",
+            work_address=work_address or "",
+            password=password,
+            profile_image=profile_image
         )
         user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
         user.is_active = True
         user.save(using=self._db)
         return user
+
 
